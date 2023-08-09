bc670017771cb4d656cce6ea32f1ffde4e5b06f4
Sowmik Shovon <sowmikshovon@gmail.com>
2023-08-10 03:42:55 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
                 <BsFillMoonStarsFill onClick={() => setDarkMode(prev => !prev)} className='cursor-pointer text-2xl' />
               </li>
               <li>
-                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="https://drive.google.com/file/d/14MACaYqKp9ayPm3VRFlsfrXyZ64jOMMY/view?usp=sharing">Resume</a>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="https://drive.google.com/file/d/11BZoCRs-wWAPRIdEwvCuZ0d8scdhWc0W/view?usp=sharing">Resume</a>
               </li>
             </ul>
           </nav>
@@ -124,7 +124,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-1 dark:text-white'>Portfolio</h3>
             <p className='text-lg py-5 leading-8 text-gray-800 dark:text-teal-200'>
-              Here I am going to highlight some of my projects that I have worked on last few months. CLick project image for live demo. You can learn more about me by clicking <a className=' text-cyan-500' href="https://drive.google.com/file/d/14MACaYqKp9ayPm3VRFlsfrXyZ64jOMMY/view?usp=sharing">here</a>.
+              Here I am going to highlight some of my projects that I have worked on last few months. CLick project image for live demo. You can learn more about me by clicking <a className=' text-cyan-500' href="https://drive.google.com/file/d/11BZoCRs-wWAPRIdEwvCuZ0d8scdhWc0W/view?usp=sharing">here</a>.
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
