a3c82a61e51330eee7475ca5de87fb2fc43ab0cd
sowmikshovon <sowmikshovon@gmail.com>
2023-08-16 21:28:45 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import Head from 'next/head';
-import { BsFillMoonStarsFill }  from 'react-icons/bs';
-import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub, AiFillFacebook } from 'react-icons/ai';
-import { MdEmail } from 'react-icons/md';
-import Image from 'next/image';
-import deved from '../public/dev-ed-wave.png';
-import design from '../public/design.png';
-import code from '../public/code.png';
-import consulting from '../public/consulting.png';
-import web1 from '../public/web1.png';
-import web2 from '../public/web2.PNG';
-import web3 from '../public/web3.PNG';
-import web4 from '../public/web4.PNG';
-import web5 from '../public/web5.PNG';
-import web6 from '../public/web6.PNG';
-import { useState } from 'react';
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillFacebook,
+} from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.PNG";
+import web3 from "../public/web3.PNG";
+import web4 from "../public/web4.PNG";
+import web5 from "../public/web5.PNG";
+import web6 from "../public/web6.PNG";
+import web7 from "../public/web7.png";
 
-
+import { useState } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,129 +32,219 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen ">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
             <h1 className="text-xl font-burtons">sowmikshovon</h1>
-            <ul className='flex items-center'>
+            <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(prev => !prev)} className='cursor-pointer text-2xl' />
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode((prev) => !prev)}
+                  className="cursor-pointer text-2xl"
+                />
               </li>
               <li>
-                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="https://drive.google.com/file/d/11BZoCRs-wWAPRIdEwvCuZ0d8scdhWc0W/view?usp=sharing">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="https://drive.google.com/file/d/1TfMGQlpCxH0UNseyow7X8Fcov7ulFOOp/view?usp=sharing"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
-          <div className='text-center p-10'>
-            <h2 className='text-5xl py-2 text-teal-600 dark:text-teal-400 font-medium md:text-6xl'>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 dark:text-teal-400 font-medium md:text-6xl">
               Sowmik Shovon Karmakar
             </h2>
-            <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>Software Developer</h3>
-            <p className='text-lg py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-xl mx-auto'>Hello. My name is <span className='text-teal-500'>Sowmik Shovon Karmakar</span>. I am a <span className='text-teal-500'>Softwere Developer</span> from Bangladesh. 
-              I graduated from BRAC University in 2022. Right now I am searching for a full time job. 
-              My expertise lies in web development, and software engineering. 
-              I have some experience in android app development. I am also very interested in DevOps and Quality Assurance.
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Software Developer
+            </h3>
+            <p className="text-lg py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl max-w-xl mx-auto">
+              Hello. My name is{" "}
+              <span className="text-teal-500">Sowmik Shovon Karmakar</span>. I
+              am a <span className="text-teal-500">Softwere Developer</span>{" "}
+              from Bangladesh. I graduated from BRAC University in 2022. Right
+              now I am searching for a full time job. My expertise lies in web
+              development, and software engineering. I have some experience in
+              android app development. I am also very interested in DevOps and
+              Quality Assurance.
             </p>
           </div>
-          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'>
-            <a href="https://www.facebook.com/sowmik.shovon"><AiFillFacebook /></a>
-            <a href="https://www.twitter.com/sowmikshovon"><AiFillTwitterCircle /></a>
-            <a href="https://www.linkedin.com/in/sowmikshovon"><AiFillLinkedin /></a>
-            <a href="https://github.com/sowmikshovon"><AiFillGithub /></a>
-            <a href='mailto:sowmikshovon@gmail.com'><MdEmail /></a>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <a href="https://www.facebook.com/sowmik.shovon">
+              <AiFillFacebook />
+            </a>
+            <a href="https://www.twitter.com/sowmikshovon">
+              <AiFillTwitterCircle />
+            </a>
+            <a href="https://www.linkedin.com/in/sowmikshovon">
+              <AiFillLinkedin />
+            </a>
+            <a href="https://github.com/sowmikshovon">
+              <AiFillGithub />
+            </a>
+            <a href="mailto:sowmikshovon@gmail.com">
+              <MdEmail />
+            </a>
           </div>
-          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
-            <Image src={deved} layout='fill' objectFit='cover'/>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={deved} layout="fill" objectFit="cover" />
           </div>
         </section>
 
         <section>
           <div>
-            <h3 className='text-3xl py-1 dark:text-white'>SKills.</h3>
-            <p className='text-lg py-2 leading-8 text-gray-800 dark:text-teal-200'>
-              I have some level of experience with Java, Python which I learned in academic settings.
-              I also have learned Javascript and its frameworks like ReactJs, TypeScript, AngulerJs by developing 
-              personal projects. I have also studied Computer Science so am confident about my knowledge of programming fundamentals. 
-              So I can learn any language and work with it flawlessly.
+            <h3 className="text-3xl py-1 dark:text-white">SKills.</h3>
+            <p className="text-lg py-2 leading-8 text-gray-800 dark:text-teal-200">
+              I have some level of experience with Java, Python which I learned
+              in academic settings. I also have learned Javascript and its
+              frameworks like ReactJs, TypeScript, AngulerJs by developing
+              personal projects. I have also studied Computer Science so am
+              confident about my knowledge of programming fundamentals. So I can
+              learn any language and work with it flawlessly.
             </p>
           </div>
-          <div className='lg:flex gap-10'>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
-
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={code} width={100} height={100} />
-              <h3 className='text-lg font-medium pt-8 pb-2'>Code</h3>
-              <p className='py-2 '>
-                Learning new languages every day.
-              </p>
-              <h4 className='py-4 text-teal-600'>Languages that I have learned</h4>
-              <p className='text-gray-800 py-1'>Java</p>
-              <p className='text-gray-800 py-1'>Python</p>
-              <p className='text-gray-800 py-1'>JavaScript</p>
-              <p className='text-gray-800 py-1'>HTML</p>
-              <p className='text-gray-800 py-1'>CSS</p>
-
+              <h3 className="text-lg font-medium pt-8 pb-2">Code</h3>
+              <p className="py-2 ">Learning new languages every day.</p>
+              <h4 className="py-4 text-teal-600">
+                Languages that I have learned
+              </h4>
+              <p className="text-gray-800 py-1">Java</p>
+              <p className="text-gray-800 py-1">Python</p>
+              <p className="text-gray-800 py-1">JavaScript</p>
+              <p className="text-gray-800 py-1">HTML</p>
+              <p className="text-gray-800 py-1">CSS</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
-
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
 
-              <h3 className='text-lg font-medium pt-8 pb-2'>Technologies</h3>
-              <p className='py-2 '>
-                These are the technology related to softare development I am familier with.
+              <h3 className="text-lg font-medium pt-8 pb-2">Technologies</h3>
+              <p className="py-2 ">
+                These are the technology related to softare development I am
+                familier with.
               </p>
-              <h4 className='py-4 text-teal-600'>Technologies</h4>
-              <p className='text-gray-800 py-1'>ReactJS</p>
-              <p className='text-gray-800 py-1'>TypeScript</p>
-              <p className='text-gray-800 py-1'>SQL</p>
-              <p className='text-gray-800 py-1'>Tailwind</p>
-              <p className='text-gray-800 py-1'>Linux</p>
+              <h4 className="py-4 text-teal-600">Technologies</h4>
+              <p className="text-gray-800 py-1">ReactJS</p>
+              <p className="text-gray-800 py-1">TypeScript</p>
+              <p className="text-gray-800 py-1">SQL</p>
+              <p className="text-gray-800 py-1">Tailwind</p>
+              <p className="text-gray-800 py-1">Linux</p>
             </div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
-
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={consulting} width={100} height={100} />
 
-              <h3 className='text-lg font-medium pt-8 pb-2'>Others</h3>
-              <p className='py-2 '>
+              <h3 className="text-lg font-medium pt-8 pb-2">Others</h3>
+              <p className="py-2 ">
                 There are many other things that I am interested in.
               </p>
-              <h4 className='py-4 text-teal-600'>DevOps</h4>
-              <h4 className='py-4 text-teal-600'>Android App Development</h4>
-              <h4 className='py-4 text-teal-600'>Photography</h4>
-              <h4 className='py-4 text-teal-600'>Random Research</h4>
-
+              <h4 className="py-4 text-teal-600">DevOps</h4>
+              <h4 className="py-4 text-teal-600">Android App Development</h4>
+              <h4 className="py-4 text-teal-600">Photography</h4>
+              <h4 className="py-4 text-teal-600">Random Research</h4>
             </div>
-            
           </div>
         </section>
-        <section className='py-10'>
+        <section className="py-10">
           <div>
-            <h3 className='text-3xl py-1 dark:text-white'>Portfolio</h3>
-            <p className='text-lg py-5 leading-8 text-gray-800 dark:text-teal-200'>
-              Here I am going to highlight some of my projects that I have worked on last few months. CLick project image for live demo. You can learn more about me by clicking <a className=' text-cyan-500' href="https://drive.google.com/file/d/11BZoCRs-wWAPRIdEwvCuZ0d8scdhWc0W/view?usp=sharing">here</a>.
+            <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
+            <p className="text-lg py-5 leading-8 text-gray-800 dark:text-teal-200">
+              Here I am going to highlight some of my projects that I have
+              worked on last few months. CLick project image for live demo. You
+              can learn more about me by clicking{" "}
+              <a
+                className=" text-cyan-500"
+                href="https://drive.google.com/file/d/11BZoCRs-wWAPRIdEwvCuZ0d8scdhWc0W/view?usp=sharing"
+              >
+                here
+              </a>
+              .
             </p>
           </div>
-          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='basis-1/3 flex-1'>
-              <a href='https://sowmikshovon.github.io/Museum-of-Candy/'><Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'} style='responsive'/></a>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmikshovon.github.io/Museum-of-Candy/">
+                <Image
+                  src={web1}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  style="responsive"
+                />
+              </a>
             </div>
-            <div className='basis-1/3 flex-1'>
-              <a href='https://sowmikshovon.github.io/gpt3_react/'><Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/></a>
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmikshovon.github.io/gpt3_react/">
+                <Image
+                  src={web2}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
-            <div className='basis-1/3 flex-1'>
-              <a href='https://sowmikshovon.github.io/restrurant_react/'><Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/></a>
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmikshovon.github.io/restrurant_react/">
+                <Image
+                  src={web3}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
-            <div className='basis-1/3 flex-1'>
-              <a href='https://sowmikshovon.github.io/spotify-clone/'><Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/></a>
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmikshovon.github.io/spotify-clone/">
+                <Image
+                  src={web4}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
-            <div className='basis-1/3 flex-1'>
-              <a href='https://sowmikshovon.github.io/portfolio_terminal/'><Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/></a>
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmikshovon.github.io/portfolio_terminal/">
+                <Image
+                  src={web5}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
-            <div className='basis-1/3 flex-1'>
-              <a href='#'><Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/></a>
+            <div className="basis-1/3 flex-1">
+              <a href="#">
+                <Image
+                  src={web6}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <a href="https://sowmik-portfolio-nextjs.vercel.app/">
+                <Image
+                  src={web7}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
           </div>
         </section>
       </main>
     </div>
-  )
+  );
 }
